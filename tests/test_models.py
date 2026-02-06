f8a320f1badc2953bd0ceca4d224d34921a35e2d
shubhaje <shubhangi.ajegaonkar@gmail.com>
2026-02-06 23:11:03 +0100

    updated models.py

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,3 +1,9 @@
+import sys
+import os
+
+# Add parent directory to path so we can import models
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from models import TestStep, TestPlan
 
 
